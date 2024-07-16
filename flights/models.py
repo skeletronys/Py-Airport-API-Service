9f9116e8c6b596571dd1f603e5bd73b11610746b
skeletronys <mykytcey@gmail.com>
2024-07-17 01:22:59 +0200

    add to models method __str__

--- flights/models.py
+++ flights/models.py
@@ -6,10 +6,16 @@ class Crew(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Airport(models.Model):
     name = models.CharField(max_length=255)
     closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -20,11 +26,17 @@ class Order(models.Model):
 class AirplaneType(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Route(models.Model):
     source = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="source_routes")
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="destination_routes")
     distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source.name} - {self.destination.name}"
 
 
 class Airplane(models.Model):
@@ -32,6 +44,9 @@ class Airplane(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
     airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE, related_name="airplanes")
+
+    def __str__(self):
+        return self.name
 
 
 class Flight(models.Model):
