97d041b41ef682f9ab3033b20b5322b72ee8aeba
skeletronys <mykytcey@gmail.com>
2024-07-16 23:46:16 +0200

    add all standart views and router in urls

--- flights/urls.py
+++ flights/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from flights.views import CrewListViewSet
+from flights.views import (
+    CrewListViewSet,
+    RouteListViewSet,
+    AirportListViewSet,
+    AirplaneListViewSet,
+    AirplaneTypeListViewSet,
+    OrderListViewSet,
+    TicketListViewSet,
+    FlightListViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register("crew", CrewListViewSet)
+router.register("route", RouteListViewSet)
+router.register("airport", AirportListViewSet)
+router.register("airplane", AirplaneListViewSet)
+router.register("airplanetype", AirplaneTypeListViewSet)
+router.register("order", OrderListViewSet)
+router.register("ticket", TicketListViewSet)
+router.register("flight", FlightListViewSet)
+
 
 app_name = "flights"
 
--- flights/views.py
+++ flights/views.py
@@ -24,3 +24,38 @@ from flights.serializers import (
 class CrewListViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+
+
+class RouteListViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class AirportListViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class AirplaneListViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class AirplaneTypeListViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class OrderListViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class TicketListViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class FlightListViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
