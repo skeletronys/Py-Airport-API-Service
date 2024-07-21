196449d607c4e83c8d625c8ddcbb9176ba443aec
skeletronys <mykytcey@gmail.com>
2024-07-21 20:03:33 +0200

    Register models in admins

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Crew, Airport, Order, AirplaneType, Route, Airplane, Flight, Ticket
 
-# Register your models here.
+admin.site.register(Crew)
+admin.site.register(Airport)
+admin.site.register(Order)
+admin.site.register(AirplaneType)
+admin.site.register(Route)
+admin.site.register(Airplane)
+admin.site.register(Flight)
+admin.site.register(Ticket)
