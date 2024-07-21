3cf82163fe0ed9607508346b459cf4539482028e
skeletronys <mykytcey@gmail.com>
2024-07-21 20:10:13 +0200

    Replace '' to ""

--- user/serializers.py
+++ user/serializers.py
@@ -5,23 +5,23 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-    confirm_password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    password = serializers.CharField(write_only=True, style={"input_type": "password"})
+    confirm_password = serializers.CharField(write_only=True, style={"input_type": "password"})
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'confirm_password', 'email')
+        fields = ("username", "password", "confirm_password", "email")
 
     def validate(self, data):
-        if data['password'] != data['confirm_password']:
+        if data["password"] != data["confirm_password"]:
             raise serializers.ValidationError("Passwords do not match")
         return data
 
     def create(self, validated_data):
         user = User(
-            username=validated_data['username'],
-            email=validated_data['email']
+            username=validated_data["username"],
+            email=validated_data["email"]
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data["password"])
         user.save()
         return user
