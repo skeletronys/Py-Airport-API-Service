37c8db35f13497cd2849e89919fb37957c38d433
skeletronys <mykytcey@gmail.com>
2024-07-23 16:36:09 +0200

    Used absolute imports

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateUserView, UserDetailView
+from user.views import CreateUserView, UserDetailView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from user.serializers import UserSerializer
 
 
 User = get_user_model()
