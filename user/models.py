75f62f26ece904f6e71bd12762332723c4a2e0b9
skeletronys <mykytcey@gmail.com>
2024-07-23 16:12:35 +0200

    Deleted all

--- user/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    username = models.CharField(max_length=150, unique=True, null=True, blank=True)
